965ca8f2d2c3b7cfb21b7e08d9601ba7160c0683
nayandaCatch <nayanda.catch@gmail.com>
2018-02-16 00:14:16 +0700

    Initial Commit

---
+++ Eatr/Eatr.h
@@ -0,0 +1,13 @@
+//
+//  Eatr.h
+//  Eatr
+//
+//  Created by Nayanda Haberty on 16/02/18.
+//  Copyright © 2018 Nayanda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Eatr : NSObject
+
+@end
---
+++ Eatr/Eatr.m
@@ -0,0 +1,13 @@
+//
+//  Eatr.m
+//  Eatr
+//
+//  Created by Nayanda Haberty on 16/02/18.
+//  Copyright © 2018 Nayanda. All rights reserved.
+//
+
+#import "Eatr.h"
+
+@implementation Eatr
+
+@end
